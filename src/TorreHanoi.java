77511840a80df14c9e1aad2ffeffe4c23a7bdf79
jfassina <132901089+jfassina@users.noreply.github.com>
2023-08-16 07:56:24 -0300

    Add files via upload

---
+++ src/TorreHanoi.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+import java.util.Scanner;
+
+public class TorreHanoi {
+
+    
+    public static int getDiscos() {
+
+        System.out.println("Digite o número de Discos: ");
+            Scanner input = new Scanner(System.in);
+
+            int numDiscos = input.nextInt();
+
+        return numDiscos;
+    }
+        public static void main(String [] args){
+            
+            Scanner input = new Scanner(System.in);
+
+           Stack A = new Stack(); 
+           Stack B = new Stack();
+           Stack C = new Stack();
+            
+            
+            
+        }
+    
+    
+}
